57e0dced268dbd96ebcc7f4359180e5b51b45898
Kousokabe Shinya <kichikichi.9016@gmail.com>
2021-04-10 20:12:32 +0900

    make class ObsControl

--- obscontrol.py
+++ obscontrol.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+import pyslobs as slobs
+import asyncio
+
+
+async def set_sceneitem_scale(conn, scene_id, item_id, width, height):
+    ss = slobs.ScenesService(conn)
+    scene = await ss.get_scene(scene_id)
+    item = await scene.get_item(item_id)
+    print(item.name)
+    await item.set_scale(slobs.IVec2(width, height))
+    await conn.close()
+
+class ObsContorol:
+
+    def set_sceneitem_scale(self, scene_id, item_id, width, height):
+        conn = slobs.SlobsConnection(slobs.config_from_ini())
+        async def main():
+            await asyncio.gather(conn.background_processing(),
+                    set_sceneitem_scale(conn, scene_id, item_id, width, height))
+        asyncio.run(main())
+
