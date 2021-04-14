1fe16e45d48a4a2215db31267818ff8c325b1818
Kousokabe Shinya <kichikichi.9016@gmail.com>
2021-04-15 08:00:32 +0900

    トランジションボタンを押したらソースサイズを調整するようになった

--- main.py
+++ main.py
@@ -3,17 +3,24 @@
 import ParticipantManager
 import ui
 import delivery
+import obscontrol
 import copy
 import utils
 
 import functools as ft
 import copy as cp
 
-def onlyOne(teams, u, desc, text):
+
+def trainsition(scene: dict):
+    for item in scene.values():
+        item.align()
+
+
+def onlyOne(teams, u, desc, text, scene):
     d = delivery._FewStreamManager(desc, 1)
 
     tab = u.addTab(text)
-    fr = ui.FrameDesignerD(tab, d.reset, lambda: print("トランジションした体"))
+    fr = ui.FrameDesignerD(tab, d.reset, ft.partial(trainsition, scene))
 
     for i in teams:
         for j in teams[i]:
@@ -24,11 +31,11 @@ def onlyOne(teams, u, desc, text):
     return tab
 
 
-def OnevOne(teams, u, desc, text):
+def OnevOne(teams, u, desc, text, scene):
     d = delivery._FewStreamManager(desc, 2)
 
     tab = u.addTab(text)
-    fr = ui.FrameDesignerB(tab, d.reset, lambda: print("トランジションした体"))
+    fr = ui.FrameDesignerB(tab, d.reset, ft.partial(trainsition, scene))
 
     for i in teams:
         for j in teams[i]:
@@ -40,11 +47,11 @@ def OnevOne(teams, u, desc, text):
     return tab
 
 
-def OneTeam(teams, u, desc, text):
+def OneTeam(teams, u, desc, text, scene):
     d = delivery._FewStreamManager(desc, 3)
 
     tab = u.addTab(text)
-    fr = ui.FrameDesignerB(tab, d.reset, lambda: print("トランジションした体"))
+    fr = ui.FrameDesignerB(tab, d.reset, ft.partial(trainsition, scene))
 
     for i in teams:
         urls = [teams[i][j].videoURL for j in teams[i] if teams[i][j] is not None]
@@ -54,7 +61,7 @@ def OneTeam(teams, u, desc, text):
     return tab
 
 
-def TowvTow(teams, u, desc, text):
+def TowvTow(teams, u, desc, text, scene):
     d = delivery._ManySteamManager(desc, 2, 2)
 
     tab = u.addTab(text)
@@ -64,7 +71,7 @@ def TowvTow(teams, u, desc, text):
 
         mem = teams[i].keys()
 
-        def cmd(team, *selmem):
+        def cmd(team, *selmem, scene):
             d.setURL(id(team), *[team[j].videoURL for j in selmem])
 
         fr.add(text=i, command=ft.partial(cmd, cp.copy(teams[i])), member=mem)
@@ -83,21 +90,22 @@ def OptionTab(u: ui.UI, text: str, listreload):
 def imptabCreate(teams: ParticipantManager.Participant, u: ui.tk.Widget):
 
     imptab = list()
+    db = obscontrol.read_db()
 
-    imptab.append(onlyOne(teams, u, 49513, '一人視点 A'))
-    imptab.append(onlyOne(teams, u, 49514, '一人視点 B'))
+    imptab.append(onlyOne(teams, u, 49513, '一人視点 A', db['1a']))
+    imptab.append(onlyOne(teams, u, 49514, '一人視点 B', db['1b']))
 
-    imptab.append(OnevOne(teams, u, 49515, '1vs1 A'))
-    imptab.append(OnevOne(teams, u, 49517, '1vs1 B'))
+    imptab.append(OnevOne(teams, u, 49515, '1vs1 A', db['2a']))
+    imptab.append(OnevOne(teams, u, 49517, '1vs1 B', db['2b']))
 
-    imptab.append(OnevOne(teams, u, 49519, '収容所 A'))
-    imptab.append(OnevOne(teams, u, 49521, '収容所 B'))
+    imptab.append(OnevOne(teams, u, 49519, '収容所 A', db['ga']))
+    imptab.append(OnevOne(teams, u, 49521, '収容所 B', db['gb']))
 
-    imptab.append(OneTeam(teams, u, 49523, '1チーム視点 A'))
-    imptab.append(OneTeam(teams, u, 49526, '1チーム視点 B'))
+    imptab.append(OneTeam(teams, u, 49523, '1チーム視点 A', db['3a']))
+    imptab.append(OneTeam(teams, u, 49526, '1チーム視点 B', db['3b']))
 
-    TowvTow(teams, u, 49529, '2vs2 A')
-    TowvTow(teams, u, 49533, '2vs2 B')
+    TowvTow(teams, u, 49529, '2vs2 A', db['4a'])
+    TowvTow(teams, u, 49533, '2vs2 B', db['4b'])
 
     return imptab
 
