f2ff8e9646f143dbd9aad7217bbc2ac68e75b05e
Kousokabe Shinya <kichikichi.9016@gmail.com>
2021-04-24 17:16:48 +0900

    直前の変更

--- streamlinkwrapper.py
+++ streamlinkwrapper.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 import subprocess
+from urllib.parse import urlparse
 
 class SLManager:
     def __init__(self):
         self.__slset = {}
 
     def start(self, desc, url):
-        proc = subprocess.Popen(['streamlink', url, '720p,720p60,480p,best' , '--player-external-http', '--player-external-http-port', str(desc), '--twitch-disable-ads'])
+        parsed_url = urlparse(url)
+        if parsed_url.netloc == 'www.youtube.com' or parsed_url.netloc == 'www.twitch.tv':
+            proc = subprocess.Popen(['streamlink', url, '720p,720p60,480p,best' , '--player-external-http', '--player-external-http-port', str(desc), '--twitch-disable-ads'])
+        else if parsed_url.netloc == 'do8w5ym3okkik.cloudfront.net':
+            proc = subprocess.Popen(['ffmpegserver.py', str(desc), url, 'Referer: https://www.mildom.com/'])
+        else:
+            proc = subprocess.Popen(['ffmpegserver.py', str(desc), url])
         self.stop(desc)
         self.__slset[desc] = proc
 
